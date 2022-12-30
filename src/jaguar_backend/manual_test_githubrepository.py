fcf12a0153695cdffda6eca312f7be7a584e6eb6
kesler20 <uchekesla@gmail.com>
2022-12-30 11:29:03 +0000

    first commit

--- src/jaguar_backend/manual_test_githubrepository.py
+++ src/jaguar_backend/manual_test_githubrepository.py
@@ -9,19 +9,20 @@ print("Testing:" + GithubRepository.__doc__)
 if __name__ == "__main__":
     github_repository = GithubRepository()
 
-    if sys.argv[1] == "git":
-        if len(sys.argv) > 2:
-            if sys.argv[2] == "t":
-                github_repository.test_and_push_to_github(sys.argv)
-            elif sys.argv[2] == "init":
-                github_repository.push_new_repo_to_github(sys.argv)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "git":
+            if len(sys.argv) > 2:
+                if sys.argv[2] == "t":
+                    github_repository.test_and_push_to_github(sys.argv)
+                elif sys.argv[2] == "init":
+                    github_repository.push_new_repo_to_github(sys.argv)
+                else:
+                    github_repository.push_new_branch_to_github(sys.argv)
             else:
-                github_repository.push_new_branch_to_github(sys.argv)
-        else:
-            print("try to run one of the following")
-            print("python _dev.py git t py  -> to run the tests ")
-            print("python _dev.py git init <target_directory> -> to initialize a repo ")
-            print("python _dev.py git <any> <target_directory> -> to initialize a new branch ")
+                print("try to run one of the following")
+                print("python _dev.py git t py  -> to run the tests ")
+                print("python _dev.py git init <target_directory> -> to initialize a repo ")
+                print("python _dev.py git <any> <target_directory> -> to initialize a new branch ")
     else:
         github_repository.push_to_github(sys.argv)
 
