b7c9e4f2552e43adf827e3e8a4a145bd558c9efb
kesler20 <uchekesla@gmail.com>
2022-12-30 16:38:10 +0000

    code: adjusted a manual test⭐

--- src/jaguar_backend/manual_test_amplifyapplication.py
+++ src/jaguar_backend/manual_test_amplifyapplication.py
@@ -5,7 +5,8 @@ import sys
 try:
     from src.jaguar_backend.amplify_application import AmplifyApplication
 except ModuleNotFoundError:
-    from amplify_application import AmplifyApplication
+    pass 
+    # from amplify_application import AmplifyApplication
 
 print("Testing:" + AmplifyApplication.__doc__)
 
@@ -15,14 +16,18 @@ except FileNotFoundError:
     pass
 
 if __name__ == "__main__":
+    amplify_application = AmplifyApplication()
+    print(amplify_application.categories)
+
+    amplify_application.initialize_amplify_application(sys.argv)
+
+    amplify_application.modify_amplify_application(sys.argv)
+
+    amplify_application.update_amplify_application(sys.argv)
+
+    amplify_application.push_to_amplify()
+
+try:
+    shutil.rmtree(os.path.join(__file__.split("\src")[0],"src","jaguar_backend","amplify"))
+except FileNotFoundError:
     pass
-    # amplify_application = AmplifyApplication()
-    # print(amplify_application.categories)
-
-    # amplify_application.initialize_amplify_application(sys.argv)
-
-    # amplify_application.modify_amplify_application(sys.argv)
-
-    # amplify_application.update_amplify_application(sys.argv)
-
-    # amplify_application.push_to_amplify()
