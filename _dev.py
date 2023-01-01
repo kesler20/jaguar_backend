dd398cf72352461486d6b6d8135b387ac6b7e1ff
kesler20 <uchekesla@gmail.com>
2023-01-01 21:29:46 +0000

    code: make it better (untested)⭐

--- _dev.py
+++ _dev.py
@@ -12,7 +12,7 @@ try:
     from src.jaguar_backend.react_application import ReactApplication
     from src.jaguar_backend.github_repository import GithubRepository
     from src.jaguar_backend._base import WorkflowRepresentation
-except ModuleNotFoundError: 
+except ModuleNotFoundError:
     from jaguar_backend.operating_system_interface import OperatingSystemInterface
     from jaguar_backend.amplify_application import AmplifyApplication
     from jaguar_backend.react_application import ReactApplication
@@ -34,12 +34,6 @@ if __name__ == "__main__":
                     git.test_and_push_to_github(sys.argv)
                 elif sys.argv[2] == "init":
                     git.push_new_repo_to_github(sys.argv)
-                elif sys.argv[2] == "describe":
-                    # ["_dev.py","git","describe","description",("repository_name")]
-                    git.add_description_to_repo(sys.argv[3])
-                    if len(sys.argv) > 4:
-                        git.add_description_to_repo(sys.argv[3],sys.argv[4])
-
                 else:
                     git.push_new_branch_to_github(sys.argv)
             else:
@@ -47,7 +41,40 @@ if __name__ == "__main__":
                 print("python _dev.py git t py -> to run the tests ")
                 print("python _dev.py git init <target_directory> -> to initialize a repo ")
                 print("python _dev.py git <any> <target_directory> -> to initialize a new branch ")
-        
+
+        elif sys.argv[1] == "github":
+            if sys.argv[2] == "issue":
+                if sys.argv[3] == "create":
+                    git.create_issue(sys.argv[4], sys.argv[5])
+
+                elif sys.argv[3] == "read":
+                    git.read_issues()
+
+                elif sys.argv[3] == "close":
+                    # ["_dev.py","github","issue","close","from_val","to_val"]
+                    if len(sys.argv) > 5:
+                        git.close_issues(int(sys.argv[4]), int(sys.argv[5]))
+                    else:
+                        git.close_issue(sys.argv[4])
+                elif sys.argv[3] == "describe":
+                    # ["_dev.py","github","issue","describe","description",("repository_name")]
+                    git.add_description_to_repo(sys.argv[4])
+                    if len(sys.argv) > 5:
+                        git.add_description_to_repo(sys.argv[4], sys.argv[5])
+
+        elif sys.argv[1] == "readme":
+            if sys.argv[2] == "create":
+                git.create_issues_from_readme()
+            if sys.argv[2] == "read":
+                git.read_todos_from_readme()
+            if sys.argv[2] == "cross":
+                git.cross_todos_from_readme(sys.argv[3])
+            else:
+                workflow_ui.pp("apply the following methods to interact with the readme 📰")
+                print("create -> to create issues from the readme")
+                print("read -> to check all the readme todos")
+                print("cross -> tick all the todos that have been completed by passing their title")
+
         elif sys.argv[1] == "aws":
             if sys.argv[2] == "init":
                 amplify.initialize_amplify_application(sys.argv)
@@ -75,63 +102,59 @@ if __name__ == "__main__":
                     'running python workflow.py "react" "init" will initialize the react application')
 
         elif sys.argv[1] == "push":
-            for dir in os.listdir(os.path.join(osi.gcu(),"protocol")):
-                with OperatingSystemInterface(os.path.join(osi.gcu(),"protocol", dir)) as op_sys:
+            for dir in os.listdir(os.path.join(osi.gcu(), "protocol")):
+                with OperatingSystemInterface(os.path.join(osi.gcu(), "protocol", dir)) as op_sys:
                     op_sys.system("python _dev.py g")
 
         elif sys.argv[1] == "install":
             workflow_ui.pp("INSTALLING JAGUAR <😼⏬>")
             # now you can push all of the changes to github within the protocol folder as follows
-            for index, dir in enumerate(os.listdir(os.path.join(osi.gcu(),"protocol"))):
+            for index, dir in enumerate(os.listdir(os.path.join(osi.gcu(), "protocol"))):
                 if dir == "jaguar_backend":
                     pass
                 else:
-                    osi = OperatingSystemInterface(os.path.join(osi.gcu(),"protocol", dir))
+                    osi = OperatingSystemInterface(os.path.join(osi.gcu(), "protocol", dir))
                     osi.copy_file_from_folder("_dev.py")
-                    if index + 1 == len(os.listdir(os.path.join(osi.gcu(),"protocol"))):
-                        workflow_ui.pp("you can now paste the src.jaguar_backend in directory 😇")
-                        os.system(f'code "" "{os.path.join(osi.gcu(),"protocol","jaguar_backend")}"')
+                    
+                    with OperatingSystemInterface(os.path.join(osi.gcu(), "protocol", dir)) as op_sys:
+                        try:
+                            # op_sys.system("rmdir /S /Q jaguar_backend")
+                            # op_sys.system("rmdir /S /Q src")
+                            op_sys.mkdir("src")
+                            op_sys.mkdir(os.path.join("src", "jaguar_backend"))
+                        except FileExistsError: 
+                            pass
+
+                    osi.copy_folder_from_folder(os.path.join("src", "jaguar_backend"))
+                    if index + 1 == len(os.listdir(os.path.join(osi.gcu(), "protocol"))):
+                        workflow_ui.pp("Installation completed 😇")
+
+        
+        elif sys.argv[1] == "copy":
+            if sys.argv[2] == "file":
+                if len(sys.argv) > 5:
+                    osi.copy_file_from_folder(sys.argv[3],sys.argv[4])
+                else:
+                    osi.copy_file_from_folder(sys.argv[3])
+            else:
+                if len(sys.argv) > 5:
+                    osi.copy_folder_from_folder(sys.argv[3],sys.argv[4])
+                else:
+                    osi.copy_folder_from_folder(sys.argv[3])
 
         elif sys.argv[1] == "test":
             git.run_tests(sys.argv)
-            
+
         elif sys.argv[1] == "-h":
-            with open(os.path.join(osi.gcu(),"protocol","jaguar","commands.txt"), "r") as f:
+            with open(os.path.join(osi.gcu(), "protocol", "jaguar", "commands.txt"), "r") as f:
                 for line in f.readlines():
                     print(line)
-        
-        elif sys.argv[1] == "issue":
-            if sys.argv[2] == "create":
-                git.create_issue(sys.argv[3], sys.argv[4])
-        
-            elif sys.argv[2] == "read":
-                git.read_issues()
-            
-            elif sys.argv[2] == "close":
-                # ["_dev.py","issue","close","from_val","to_val"]
-                if len(sys.argv) > 4:
-                    git.close_issues(int(sys.argv[3]), int(sys.argv[4]))
-                else:
-                    git.close_issue(sys.argv[3])
-        
-        elif sys.argv[1] == "readme":
-            if sys.argv[2] == "create":
-                git.create_issues_from_readme()
-            if sys.argv[2] == "read":
-                git.read_todos_from_readme()
-            if sys.argv[2] == "cross":
-                git.cross_todos_from_readme(sys.argv[3])
-            else:
-                workflow_ui.pp("apply the following methods to interact with the readme 📰")
-                print("create -> to create issues from the readme")
-                print("read -> to check all the readme todos")
-                print("cross -> tick all the todos that have been completed by passing their title")
 
         else:
             # if no domain is passed this will be pushed to github
             git.push_to_github(sys.argv)
 
     else:
-        with open(os.path.join(osi.gcu(),"Protocol","jaguar","commands.txt"), "r") as f:
+        with open(os.path.join(osi.gcu(), "Protocol", "jaguar", "commands.txt"), "r") as f:
             for line in f.readlines():
                 print(line)
--- src/jaguar_backend/operating_system_interface.py
+++ src/jaguar_backend/operating_system_interface.py
@@ -4,10 +4,11 @@ import os
 import shutil
 import sys
 
+
 class OperatingSystemInterface:
     """OperatingSystemInterface is a class
     you can access the interface like a resource manager such as
-    
+
     ```python
     with OperatingSystemInterface(directory) as osi:
         osi.do_something()
@@ -17,7 +18,7 @@ class OperatingSystemInterface:
         oi.system("echo hello world")
     ```
     """
-    
+
     def __init__(self, directory=os.getcwd()) -> None:
         self.directory: str = directory
 
@@ -29,33 +30,33 @@ class OperatingSystemInterface:
     def __exit__(self, *args) -> os:
         '''signature description'''
         os.chdir(os.getcwd())
-      
+
     def gcf(self) -> str:
         """returns the current folder where the file is being run"""
         if sys.platform == "linux":
             return __file__.split("protocol")[1].split(r"/")[1]
         else:
-            return __file__.split("protocol")[1].split(r"\ ".replace(" ",""))[1]
+            return __file__.split("protocol")[1].split(r"\ ".replace(" ", ""))[1]
 
     def gcu(self) -> str:
         """gcu has the following params"""
         '''Get the current user i.e. C:/Users/CBE-User 05'''
-        return os.path.abspath(__file__).split(r"\protocol")[0]     
-                  
-    def copy_file_from_folder(self, file : str, source_folder: Optional[str] ="jaguar_backend") -> None:
+        return os.path.abspath(__file__).split(r"\protocol")[0]
+
+    def copy_file_from_folder(self, file: str, source_folder: Optional[str] = "jaguar_backend") -> None:
         """The folder that you are currently working on will be used as destination file
         The source folder will be searched in the protocol folder and is jaguar_backend by default
         the file which will be replace in the local directory has path 
         ``os.path.join(self.directory,file)``
-        
+
         Parameters
         ---
-            
+
         file str
             the file that we want to move to the root directory from the source_folder
         source_folder : str
             the folder where the file will be searched, this is jaguar_backend by default
-        
+
         Returns
         ---
         result: None
@@ -71,20 +72,51 @@ class OperatingSystemInterface:
         {}
         '''.format(source, destination))
         print(os.getcwd())
-        shutil.copy(source, destination)        
-    
-    def move_folder_resources(self,source_path: str, destination_path : str) -> None:
-        """move_folder_resources 
-        the directory passed as a property will be used as a source path
-        
+        shutil.copy(source, destination)
+
+    def copy_folder_from_folder(self, folder: str, source_folder: Optional[str] = os.path.join("protocol", "jaguar_backend")) -> None:
+        """The folder that you are currently working on will be used as destination folder
+        The source folder will be searched in the protocol folder and is protocol by default
+        the folder which will be replace in the local directory has path 
+        ``os.path.join(self.directory,folder)``
+
         Parameters
         ---
-            
+
+        folder str
+            the folder that we want to move to the root directory from the source_folder
+        source_folder : str
+            the folder where the folder will be searched, this is protocol by default
+
+        Returns
+        ---
+        result: None
+        """
+
+        source = os.path.join(os.path.abspath(__file__).split(
+            r"\{}".format(source_folder))[0], source_folder, folder)
+        destination = os.path.join(self.directory, folder)
+
+        print(r'''
+        copying {} 
+        ---> into 
+        {}
+        '''.format(source, destination))
+        print(os.getcwd())
+        shutil.copytree(source, destination)
+
+    def move_folder_resources(self, source_path: str, destination_path: str) -> None:
+        """move_folder_resources 
+        the directory passed as a property will be used as a source path
+
+        Parameters
+        ---
+
         destination_path str
             this is the folder where the files will be moved to
         source_path str
             this is the folder where the files will be moved from
-        
+
         Returns
         ---
         result: None
@@ -93,16 +125,16 @@ class OperatingSystemInterface:
             destination_dir = os.path.join(destination_path, resource)
             source_dir = os.path.join(source_path, resource)
             os.rename(source_dir, destination_dir)
-                          
-    def read_word_in_directory(self, word : str) -> 'list[str]':
+
+    def read_word_in_directory(self, word: str) -> 'list[str]':
         """read_word_in_directory has the following params
-        
+
         Parameters
         ---
-            
+
         word str
             The word that will be searched on the current directory
-        
+
         Example
         ---
 
@@ -120,7 +152,7 @@ class OperatingSystemInterface:
         result = []
         for root, directories, files in os.walk(self.directory):
             for file in files:
-                with open(os.path.join(self.directory,file)) as f:
+                with open(os.path.join(self.directory, file)) as f:
                     content = f.read()
                     if content.find(word) != -1:
                         result.append(file)
